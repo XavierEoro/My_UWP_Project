ef999b9112b6d4ef6a651be7b5ec572628ac7aa1
v-doxie <v-doxie@VDI-V-DOXIE>
2017-02-21 17:31:19 +0800

    add call C# method in html js function sample

---
+++ MyUWPProject/AppTestSample/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using HtmlCommunicator;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AppTestSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.HtmlWebView.Navigate(new Uri("ms-appx-web:///HTMLPage1.html"));
+        }
+
+        HtmlCommunicators communicationWinRT = new HtmlCommunicators();
+        private void HtmlWebView_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            this.HtmlWebView.AddWebAllowedObject("SCObject", communicationWinRT);
+        }
+    }
+}
---
+++ MyUWPProject/HtmlCommunicator/HtmlCommunicators.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Foundation.Metadata;
+
+namespace HtmlCommunicator
+{
+      [AllowForWeb]
+        public sealed class HtmlCommunicators
+        {
+            public string getAppVersion()
+            {
+                PackageVersion version = Package.Current.Id.Version;
+                return String.Format("{0}.{1}.{2}.{3}", version.Major, version.Minor, version.Build, version.Revision);
+            }
+        }
+    
+}
