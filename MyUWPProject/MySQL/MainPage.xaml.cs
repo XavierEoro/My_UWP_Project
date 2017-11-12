18a45cee6b49bb9bc18daa03664527a83a709f36
xiedongwei <xiedongwei@live.cn>
2017-11-12 13:47:42 +0800

    add mysql uwp sample

---
+++ MyUWPProject/MySQL/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MySQL
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public void getmysqlread()
+        {
+            string M_str_sqlcon = "server=10.xxx.xx.xxx;user id=root;password=xxx;database=uwptest";
+            MySqlConnection mysqlcon = new MySqlConnection(M_str_sqlcon);
+            MySqlCommand mysqlcom = new MySqlCommand("select * from table1", mysqlcon);
+            mysqlcon.Open();
+            MySqlDataReader mysqlread = mysqlcom.ExecuteReader(CommandBehavior.CloseConnection);
+            while (mysqlread.Read())
+            {
+                Debug.WriteLine(mysqlread.GetString(0) + ":" + mysqlread.GetString(1));
+            }
+            mysqlcon.Close();
+        }
+    }
+}
